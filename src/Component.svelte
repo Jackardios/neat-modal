33a8b67aa624a5ba7be6a1f81b5e0c0a38a3b1af
Salavat Salakhutdinov <salahutdinov.salavat@gmail.com>
2021-04-28 03:36:27 +0300

    change file structure

--- src/Component.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-    const text = "Hello";
-</script>
-
-<h1>{text}</h1>
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export { default } from './Component.svelte';
+export { default as NeatModal } from './components/NeatModal.svelte';
